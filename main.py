3a75cb0511070c828423a459e40b4c834ab4ede4
Ashwath S <miniash123@outlook.com>
2025-12-16 21:33:19 +0530

    made the index page work using jinja templates

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from urllib.parse import quote
 
+import sqlalchemy
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
@@ -44,7 +45,14 @@ db.init_app(app)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    global all_movies
+    try:
+        print("==================> Getting all movies")
+        all_movies = Movie.query.all()
+        print(all_movies)
+    except Exception as e:
+        print(f"Error Getting records = {e}")
+    return render_template("index.html", movies=all_movies)
 
 
 if __name__ == '__main__':
@@ -52,4 +60,43 @@ if __name__ == '__main__':
         print("===========================> Creating tables")
         db.create_all()
         print("============================> Finished Creating tables")
+
+        # try:
+        #     print("======================> adding 1st movie")
+        #     new_movie = Movie(
+        #         title="Phone Booth",
+        #         year=2002,
+        #         description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+        #         rating=7.3,
+        #         ranking=10,
+        #         review="My favourite character was the caller.",
+        #         img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+        #     )
+        #
+        #     db.session.add(new_movie)
+        #     db.session.commit()
+        #     print("=========================> finished adding 1st movie")
+        #
+        # except sqlalchemy.exc.IntegrityError as e:
+        #     print("=================> 1st movie is already in the table")
+
+        # try:
+        #     print("========================> adding 2nd movie")
+        #     second_movie = Movie(
+        #         title="Avatar The Way of Water",
+        #         year=2022,
+        #         description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+        #         rating=7.3,
+        #         ranking=9,
+        #         review="I liked the water.",
+        #         img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+        #     )
+        #
+        #     db.session.add(second_movie)
+        #     db.session.commit()
+        #     print("========================> finished adding 2nd movie")
+        #
+        # except sqlalchemy.exc.IntegrityError as e:
+        #     print("=================> 2nd movie is already in the table")
+
     app.run(debug=True)
